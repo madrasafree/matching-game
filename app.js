e320de5c2784105f2843f52914ec0d0d26974ebf
widad boulos <waddu@Widads-Mac.local>
2021-12-03 17:36:55 +0200

    Bug Fix: Fix the case of an error and the case of choosing the same card twice
    Signed-off-by: widad boulos <waddu@Widads-Mac.local>

--- app.js
+++ app.js
@@ -4,30 +4,43 @@ $(".board").css("display", "none");
 
 $(".card").each(function (index, card) {
   $(card).on("click", function (e) {
-    if (currentCard) {
-      if (
-        $(this).data("match") === currentCard.data("match") &&
-        $(this).data("group") === currentCard.data("group")
-      ) {
-        $(this).css("outline", "none");
-        currentCard = undefined;
-      } else if ($(this).data("match") === currentCard.data("match")) {
-        let match = $(".match-" + $(this).data("match"));
+    if (currentCard) { // if current card is the past card and "this" is the current card
+      if ($(this).data("match") === currentCard.data("match") &&
+        $(this).data("group") === currentCard.data("group")) {// if we picked the same card twice
+            
+        $(this).css("outline", "2px solid orange");
+        $(this).css("background", "#F7CD7C");
+        currentCard = $(this);
 
-        match.css("outline", "2px solid green").css("background", "lightgreen");
-        count++;
+      } else if ($(this).data("match") === currentCard.data("match")) {// if we picked the right card
 
-        setTimeout(function () {
-          match.css("visibility", "hidden");
-          currentCard = undefined;
-          if (count == 8) {
-            $(".board").css("display", "none");
-            $(".end").css("display", "block");
-          }
-        }, 250);
-      } else {
+            let match = $(".match-" + $(this).data("match"));
+            match.css("outline", "2px solid green").css("background", "lightgreen");
+            count++;
+
+            setTimeout(function () {
+              match.css("visibility", "hidden");
+              currentCard = undefined;
+              if (count == 8) {
+                $(".board").css("display", "none");
+                $(".end").css("display", "block");
+              }
+            }, 250);
+      } else {// if wrong
+            $(this).css("outline", "2px solid red");
+            $(this).css("background", "white");
+            currentCard.css("outline", "2px solid red");
+            currentCard.css("background", "white");
+            let thisCard = $(this);
+            //after a while we should remove the red on the cards
+            setTimeout(function() {
+               currentCard.css("outline", "2px solid white");
+               thisCard.css("outline", "2px solid white");
+               currentCard = undefined;
+            }, 250);
+
       }
-    } else {
+    } else { // we picked the first card and currenCard is none
       $(this).css("outline", "2px solid orange");
       $(this).css("background", "#F7CD7C");
       currentCard = $(this);
