c41dcb9703463267dc055c999c5ecbb476bcb421
pliat963 <43778199+pliat963@users.noreply.github.com>
2022-06-16 13:34:19 +0300

    fix audio

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let currentCard;
 let count = 0;
-let timeCount=0;
+let timeCount = 0;
 let entered_before = false;
 
 
@@ -59,7 +59,7 @@ function getParam(param) {
 
 //for game of matching audio to hebrew words
 let game_type = getParam('media');
-if(game_type === "audio"){
+if (game_type === "audio") {
   $('.instructions').text('התאימו את ההקלטות בערבית לפירוש העברי ');
 }
 
@@ -93,7 +93,7 @@ function filtereWords(Words, units_array) {
 
   for (let i = 0; i < units_array.length; i += 2) {
     tmp = filterWordsByLesson(Words, units_array[i]);
-    tmp = filterWordsByUnits(tmp, units_array[i+1]);
+    tmp = filterWordsByUnits(tmp, units_array[i + 1]);
     let merged = filteringWords.concat(tmp);
     filteringWords = merged;
   }
@@ -101,7 +101,7 @@ function filtereWords(Words, units_array) {
   return filteringWords;
 }
 
-function lesson_parser (Words, lessons) {
+function lesson_parser(Words, lessons) {
   if (lessons) {
     let lessons_array = lessons.split('$');
     let units_array = [];
@@ -143,14 +143,14 @@ var filteredWords = getRandomWords(
 //pick random indexes:
 function shuffle(array) {
   var i = array.length,
-      j = 0,
-      temp;
+    j = 0,
+    temp;
   while (i--) {
-      j = Math.floor(Math.random() * (i+1));
-      // swap randomly chosen element with current element
-      temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+    j = Math.floor(Math.random() * (i + 1));
+    // swap randomly chosen element with current element
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
   return array;
 }
@@ -162,60 +162,60 @@ var words_array = [];
 var audio_array = []
 
 $.each(filteredWords, function (i, word) {
-    words_array.push(word["hebrew"]);
-    words_array.push(word["arabic"]);
-    audio_array.push(word["audio"]);
+  words_array.push(word["hebrew"]);
+  words_array.push(word["arabic"]);
+  audio_array.push(word["audio"]);
 
 });
 
 
 $.each(words_array, function (i, word) {
-var d1 = document.getElementById("cards-container")
-let counter1 = -1;
-let audio_counter = -1;
-let lang = "";
-let curr_audio;
-let style_font_size = ""
+  var d1 = document.getElementById("cards-container")
+  let counter1 = -1;
+  let audio_counter = -1;
+  let lang = "";
+  let curr_audio;
+  let style_font_size = ""
 
-for (const element of array_of_random_indexes) {
-  counter1++;
-  if((counter1 % 2) == 1){
-    audio_counter ++;
-    curr_audio = audio_array[audio_counter];
-  }
-  if (element == i) {
-    if (words_array[counter1].length > 26) {
-      style_font_size = "style=\"font-size: 20px; \" "
-    } else if (words_array[counter1].length > 18) {
-      style_font_size = "style=\"font-size: 22px;\" "
-    } else {
-      style_font_size = "style=\"font-size: 25px;\" "
-    }
+  for (const element of array_of_random_indexes) {
+    counter1++;
     if ((counter1 % 2) == 1) {
-      lang = "arabic";
-      if(game_type === "audio"){
-        d1.insertAdjacentHTML("beforeend",
-        "<div class=" + "\"card match-" + array_of_data[counter1] + "\"" + "data-match=" + array_of_data[counter1] + " data-group=" + "\"" + lang + "\"" + 
-        " data-audio=" + curr_audio + "> <img src=\"images/audio_icon.png\" width=\"60\" alt=\"play audio\" />  </div>"
-        );
+      audio_counter++;
+      curr_audio = audio_array[audio_counter];
+    }
+    if (element == i) {
+      if (words_array[counter1].length > 26) {
+        style_font_size = "style=\"font-size: 20px; \" "
+      } else if (words_array[counter1].length > 18) {
+        style_font_size = "style=\"font-size: 22px;\" "
+      } else {
+        style_font_size = "style=\"font-size: 25px;\" "
+      }
+      if ((counter1 % 2) == 1) {
+        lang = "arabic";
+        if (game_type === "audio") {
+          d1.insertAdjacentHTML("beforeend",
+            "<div class=" + "\"card match-" + array_of_data[counter1] + "\"" + "data-match=" + array_of_data[counter1] + " data-group=" + "\"" + lang + "\"" +
+            " data-audio=" + curr_audio + "> <img src=\"images/audio_icon.png\" width=\"60\" alt=\"play audio\" />  </div>"
+          );
+
+        } else {
+          d1.insertAdjacentHTML("beforeend",
+            "<div " + style_font_size + "class=" + "\"card match-" + array_of_data[counter1] + "\"" + "data-match=" + array_of_data[counter1] + " data-group=" + "\"" + lang + "\"" +
+            " data-audio=" + curr_audio + "> " + words_array[counter1] + "</div>"
+          );
+        }
 
       } else {
+        lang = "hebrew";
         d1.insertAdjacentHTML("beforeend",
-        "<div " + style_font_size + "class=" + "\"card match-" + array_of_data[counter1] + "\"" + "data-match=" + array_of_data[counter1] + " data-group=" + "\"" + lang + "\"" + 
-        " data-audio=" + curr_audio + "> " + words_array[counter1] + "</div>"
+          "<div " + style_font_size + "class=" + "\"card match-" + array_of_data[counter1] + "\"" + "data-match=" + array_of_data[counter1] + " data-group=" + "\"" + lang + "\"" + ">" + words_array[counter1] + "</div>"
         );
       }
-     
-    } else {
-      lang = "hebrew";
-        d1.insertAdjacentHTML("beforeend",
-        "<div " + style_font_size + "class=" + "\"card match-" + array_of_data[counter1] + "\"" + "data-match=" + array_of_data[counter1] + " data-group=" + "\"" + lang + "\"" + ">" + words_array[counter1] + "</div>"
-        );
     }
   }
-}
 
-counter1 = -1;
+  counter1 = -1;
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -224,82 +224,95 @@ counter1 = -1;
 // code of the game:
 $(".card").each(function (index, card) {
   $(card).on("click", function (e) {
-    if(game_type === "audio"){
-      if($(this).data("group") == "arabic"){
-        let audio1 = $(this).data("audio");
-        let audio2 = new Audio(audio1);
-        audio2.play();
-      }
-    }
-  
+
     if (currentCard) { // if current card is the past card and "this" is the current card
+      // this is the second click on a card
+
+      if (game_type === "audio") {
+        //if currentcurd (which is the past card) was not arabic, we want to play the second (current). 
+        if (currentCard.data("group") != "arabic") {
+          if ($(this).data("group") == "arabic") {
+            let audio1 = $(this).data("audio");
+            let audio2 = new Audio(audio1);
+            audio2.play();
+          }
+        }
+      }
       const cards = document.querySelectorAll('.card');
 
-      cards.forEach(card => {
+      cards.forEach(card => { //disable clicking on more cards until the game continues
         card.style.pointerEvents = 'none';
       });
 
 
       if ($(this).data("match") === currentCard.data("match") && $(this).data("group") === currentCard.data("group")) {
-            // if we picked the same card twice
-            $(this).css("outline", "2px solid orange");
-            $(this).css("background", "#F7CD7C");
-            currentCard = undefined;
-            $(this).css("background", "white");
-            $(this).css("outline", "white");
-            cards.forEach(card => {
-              card.style.pointerEvents = 'auto';
-            });
+        // if we picked the same card twice
+        $(this).css("outline", "2px solid orange");
+        $(this).css("background", "#F7CD7C");
+        currentCard = undefined;
+        $(this).css("background", "white");
+        $(this).css("outline", "white");
+        cards.forEach(card => { //enable clicking on more cards
+          card.style.pointerEvents = 'auto';
+        });
 
       } else if ($(this).data("match") === currentCard.data("match")) {
-            // if we picked the right card
-            let match = $(".match-" + $(this).data("match"));
-            match.css("outline", "2px solid green").css("background", "lightgreen");
-            count++;
+        // if we picked the right card
+        let match = $(".match-" + $(this).data("match"));
+        match.css("outline", "2px solid green").css("background", "lightgreen");
+        count++;
 
-            setTimeout(function () {
-              match.css("visibility", "hidden");
-              currentCard = undefined;
+        setTimeout(function () {
+          match.css("visibility", "hidden");
+          currentCard = undefined;
 
-              if (count == 8) {
-                clearInterval ( timer );
-                document.getElementById("finish_timer").textContent = document.getElementById("timer").textContent;
-                document.getElementById("finish_seconds").textContent = document.getElementById("seconds").textContent;
-                $("#cards-container").css("display", "none");
-                $(".end").css("display", "block");
-              }
-              cards.forEach(card => {
-                card.style.pointerEvents = 'auto';
-              });
-            }, 250);
+          if (count == 8) {
+            clearInterval(timer);
+            document.getElementById("finish_timer").textContent = document.getElementById("timer").textContent;
+            document.getElementById("finish_seconds").textContent = document.getElementById("seconds").textContent;
+            $("#cards-container").css("display", "none");
+            $(".end").css("display", "block");
+          }
+          cards.forEach(card => { //enable clicking on more cards
+            card.style.pointerEvents = 'auto';
+          });
+        }, 250);
 
       } else { // if wrong
-            $(this).css("outline", "2px solid red");
-            $(this).css("background", "red");
-            currentCard.css("outline", "2px solid red");
-            currentCard.css("background", "red");
-            var thisCard = $(this);
-            currentCard.css("animation", "shake 0.5s");
-            thisCard.css("animation", "shake 0.5s");
+        $(this).css("outline", "2px solid red");
+        $(this).css("background", "red");
+        currentCard.css("outline", "2px solid red");
+        currentCard.css("background", "red");
+        var thisCard = $(this);
+        currentCard.css("animation", "shake 0.5s");
+        thisCard.css("animation", "shake 0.5s");
 
-            //after a while we should remove the red on the cards
-            setTimeout(function() {
-               currentCard.css("outline", "2px solid white");
-               currentCard.css("background", "white");
-               thisCard.css("background", "white");
-               thisCard.css("outline", "2px solid white");
-               currentCard.css("animation", "still");
-               thisCard.css("animation", "still");
-               currentCard = undefined;
+        //after a while we should remove the red on the cards
+        setTimeout(function () {
+          currentCard.css("outline", "2px solid white");
+          currentCard.css("background", "white");
+          thisCard.css("background", "white");
+          thisCard.css("outline", "2px solid white");
+          currentCard.css("animation", "still");
+          thisCard.css("animation", "still");
+          currentCard = undefined;
 
-               cards.forEach(card => {
-               card.style.pointerEvents = 'auto';
+          cards.forEach(card => { //enable clicking on more cards
+            card.style.pointerEvents = 'auto';
 
-              });
-            }, 500);
+          });
+        }, 500);
 
       }
     } else { // we picked the first card and currenCard is none
+      if (game_type === "audio") {
+        if ($(this).data("group") == "arabic") {
+          let audio1 = $(this).data("audio");
+          let audio2 = new Audio(audio1);
+          audio2.play();
+        }
+      }
+
       $(this).css("outline", "2px solid orange");
       $(this).css("background", "#F7CD7C");
       currentCard = $(this);
@@ -309,24 +322,24 @@ $(".card").each(function (index, card) {
 
 
 //new click when we want to restart the game
-$(".restart-btn").on("click", e=> location.reload());
+$(".restart-btn").on("click", e => location.reload());
 
 //new restart when we want to restart the game when it ends
-$(".another_game-btn").on("click", e=> location.reload());
+$(".another_game-btn").on("click", e => location.reload());
 
 
 // to show all the words in the end
 $("#words_display-btn").click(function () {
 
-    let words_display = document.getElementById("words_learned");
+  let words_display = document.getElementById("words_learned");
 
-    if (!entered_before) {
-      $.each(filteredWords, function (i, word) {
-        words_display.insertAdjacentHTML("beforeend",
-          "<div>" + word["hebrew"] +  "   =   " + word["arabic"] + "</div>"
-        );
-      });
-    }
+  if (!entered_before) {
+    $.each(filteredWords, function (i, word) {
+      words_display.insertAdjacentHTML("beforeend",
+        "<div>" + word["hebrew"] + "   =   " + word["arabic"] + "</div>"
+      );
+    });
+  }
 
   $("#words_learned").css("display", "block");
 
@@ -338,8 +351,8 @@ $("#words_display-btn").click(function () {
 
 
 $("#words_hide-btn").click(function () {
-    $("#words_learned").css("display", "none");
-    $('#words_display-btn').removeAttr("disabled");
-    $('#words_hide-btn').css("display", "none");
+  $("#words_learned").css("display", "none");
+  $('#words_display-btn').removeAttr("disabled");
+  $('#words_hide-btn').css("display", "none");
 });
 
